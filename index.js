4ac162da65ecdc31100c854f5a0fd9fd79b64066
Vinayak Arora <vinayakarora7461@gmail.com>
2025-01-12 12:38:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) => {
+    res.send('vinayakdotcom')
+})
+
+app.get('/login',(req,res) => {
+    res.send('<h1> please login</h1>')
+})
+app.get('/youtube',(req,res) => {
+    res.send('<h2>vinayak the lord</h2>')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
